81c92eefaa69d13f90bf4b7f1b07c98480a3994c
ricardsupenieks <upenieksrica@gmail.com>
2023-02-01 13:59:43 +0200

    import schema.sql migration

---
+++ database/migrations/2023_01_22_100755_import_sql_dump.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        DB::unprepared( file_get_contents( "schema.sql" ));
+    }
+};
